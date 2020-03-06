1f18ac105964f5e55c7db3faa4f2eb5f153a58be
schkiho <schkiho@yahoo.de>
2020-03-06 12:16:07 +0100

    add es6 arrow function with async await

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const url =
-  'https://uberall.com/api/search/?public_key=GNpPyrkYiTS5BV4F8XckW3kYurprwSk7cRG3Z4jOtrTTBceyfPveAOvDFk3mYY0ofundf';
+// Classic way using .then & .catch
+function searchBusiness(name, street, zip, country) {
+  const url =
+    'https://uberall.com/api/search/?public_key=GNpPyrkYiTS5BV4F8XckW3kYurprwSk7cRG3Z4jOtrTTBceyfPveAOvDFk3mYY0ofundf';
 
-async function searchBusiness(name, street, zip, country) {
   const searchData = {
     name: name,
     country: country,
@@ -25,4 +26,34 @@ async function searchBusiness(name, street, zip, country) {
     });
 }
 
-console.log(searchBusiness('Saturn', 'Elsenstr. 111 - 114', ' 12435', 'DE'));
+searchBusiness('Saturn', 'Elsenstr. 111 - 114', ' 12435', 'DE');
+
+// ES6 using async await with try/catch
+searchBusiness2 = async (name, street, zip, country) => {
+  const url =
+    'https://uberall.com/api/search/?public_key=GNpPyrkYiTS5BV4F8XckW3kYurprwSk7cRG3Z4jOtrTTBceyfPveAOvDFk3mYY0ofundf';
+
+  const searchData = {
+    name: name,
+    country: country,
+    street: street,
+    zip: zip
+  };
+
+  try {
+    let response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+      },
+      body: JSON.stringify(searchData)
+    });
+    let result = await response.json();
+    console.log(result.response.searchData);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+};
+
+searchBusiness2('Saturn', 'Elsenstr. 111 - 114', ' 12435', 'DE');
